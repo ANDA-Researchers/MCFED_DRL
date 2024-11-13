fix channel, update the gradient scaling for the shared network

module/bdqn.py
@@ -89,6 +89,10 @@ class BDQN(nn.Module):
             nn.ReLU(),
         )
 
+        for layer in self.common:
+            if isinstance(layer, nn.Linear):
+                layer.register_full_backward_hook(self._backward_hook)
+
         self.advantage_branches = nn.ModuleList(
             [nn.Linear(hidden_dim, action_dim) for n in range(num_actions)]
         )
@@ -100,14 +104,13 @@ class BDQN(nn.Module):
                 nn.Linear(hidden_dim, 1),
             )
 
-    # def _register_gradient_rescaling_hook(self):
-    #     def rescale_gradients(module, grad_input, grad_output):
-    #         scale_factor = 1.0 / (self.num_actions + 1)
-    #         return tuple(
-    #             gi * scale_factor if gi is not None else None for gi in grad_input
-    #         )
+    def _backward_hook(self, module, grad_input, grad_output):
+        scale_factor = 1 / self.num_actions
 
-    #     self.common.register_backward_hook(rescale_gradients)
+        scaled_grad_input = tuple(
+            grad * scale_factor if grad is not None else None for grad in grad_input
+        )
+        return scaled_grad_input
 
     def forward(self, state):
         out = self.common(state)

simulation/channel.py
@@ -4,19 +4,17 @@ import numpy as np
 class V2X:
     def __init__(self, args=None) -> None:
         self.B_sub = 1e6  # 1 MHz
-        self.B_bs = 540e3  # 540 kHz
+        self.B_bs = 1e6  # 540 kHz
         self.P_bs = 43
         self.P_rsu = 30
         self.sigma2 = -114
-        self.shadow_std = 4
-        self.decorrelation_distance = 50
+        self.shadow_std_bs = 8
+        self.shadow_std_rsu = 3
+        self.decorrelation_distance_bs = 50
+        self.decorrelation_distance_rsu = 25
 
-        self.h_bs = 25 - 1.5  # antenna height of BS
-        self.h_rsu = 5 - 1.5  # antenna height of RSU
-
-        self.v_atten_gain = 3  # vehicle antenna gain
-        self.r_atten_gain = 5
-        self.bs_atten_gain = 14  # BS antenna gain
+        self.h_bs = 15 - 1.5
+        self.h_rsu = 5 - 1.5
 
         self.noise_power = 10 ** (self.sigma2 / 10)
 
@@ -38,14 +36,38 @@ class V2X:
         the second column is the distance between vehicle and their corresponding RSU
         """
 
-        shadowing_values = np.random.normal(
-            0, self.shadow_std, (self.distance.shape[0], 2)
+        # shadowing_values = np.random.normal(
+        #     0, self.shadow_std, (self.distance.shape[0], 2)
+        # )
+
+        shadowing_values = np.zeros((self.distance.shape[0], 2))
+        shadowing_values[:, 0] = np.random.normal(
+            0, self.shadow_std_bs, self.distance.shape[0]
         )
+        shadowing_values[:, 1] = np.random.normal(
+            0, self.shadow_std_rsu, self.distance.shape[0]
+        )
+
+        self.decorrelation_distance = np.array(
+            [self.decorrelation_distance_bs, self.decorrelation_distance_rsu]
+        )
+        # delta_distances = self.distance / self.decorrelation_distance
         delta_distances = self.distance / self.decorrelation_distance
+
+        # shadowing_decay = np.exp(-delta_distances)
         shadowing_decay = np.exp(-delta_distances)
-        additional_shadowing = np.sqrt(
-            1 - np.exp(-2 * delta_distances)
-        ) * np.random.normal(0, self.shadow_std, (self.distance.shape[0], 2))
+
+        # additional_shadowing = np.sqrt(
+        #     1 - np.exp(-2 * delta_distances)
+        # ) * np.random.normal(0, self.shadow_std, (self.distance.shape[0], 2))
+        additional_shadowing = np.zeros((self.distance.shape[0], 2))
+        additional_shadowing[:, 0] = np.sqrt(
+            1 - np.exp(-2 * delta_distances[:, 0])
+        ) * np.random.normal(0, self.shadow_std_bs, self.distance.shape[0])
+        additional_shadowing[:, 1] = np.sqrt(
+            1 - np.exp(-2 * delta_distances[:, 1])
+        ) * np.random.normal(0, self.shadow_std_rsu, self.distance.shape[0])
+
         shadowing_values = shadowing_values * shadowing_decay + additional_shadowing
         return shadowing_values
 
@@ -88,7 +110,6 @@ class V2X:
                 - path_loss_values[:, 0]
                 - shadowing_values[:, 0]
                 - fast_fading_values[:, 0]
-                + 6
             )
             / 10
         )
@@ -99,20 +120,18 @@ class V2X:
                 - path_loss_values[:, 1]
                 - shadowing_values[:, 1]
                 - fast_fading_values[:, 1]
-                + 6
             )
             / 10
         )
 
         self.channel_gain = np.concatenate(
-            [channel_gain_bs.reshape(1, -1), channel_gain_rsu.reshape(1, -1)], axis=0
+            [channel_gain_bs.reshape(-1, 1), channel_gain_rsu.reshape(-1, 1)], axis=-1
         )
-
         data_rate_bs = self.B_bs * np.log2(1 + channel_gain_bs / self.noise_power)
         data_rate_rsu = self.B_sub * np.log2(1 + channel_gain_rsu / self.noise_power)
 
         self.data_rate = np.concatenate(
-            [data_rate_bs.reshape(1, -1), data_rate_rsu.reshape(1, -1)], axis=0
+            [data_rate_bs.reshape(-1, 1), data_rate_rsu.reshape(-1, 1)], axis=-1
         )
 
         return self.channel_gain, self.data_rate